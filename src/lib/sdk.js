ad1643fd2402fcf2879cf333996eb1abda136ab6
Jeff Ellis <jellis777@gmail.com>
2025-10-01 11:17:28 -0500

    updated file names

--- src/lib/sdk.js
+++ src/lib/sdk.js
@@ -39,20 +39,3 @@ export const replaceFavorite = async (id, newFav) => {
   });
   return response.status;
 };
-
-const newFavId = await addFavorite('example', 'example');
-console.log(newFavId);
-
-let result = await getFavorite(newFavId);
-console.log(result);
-
-result.name = 'test-new';
-await replaceFavorite(result.id, result);
-
-const fav = await getFavorite(result.id);
-console.log('updated:', fav);
-
-await deleteFavorite(newFavId);
-
-result = await getFavorite(newFavId);
-console.log(result);
